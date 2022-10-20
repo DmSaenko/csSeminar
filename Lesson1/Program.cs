076981feec9c439445d58cd5685803fa9348a500
Dmitry Saenko <dm.saenko@gmail.com>
2022-10-20 15:11:29 +0300

    New file

--- Lesson1/Program.cs
+++ Lesson1/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+// Запрос чисел от пользователя
+
+Console.WriteLine("Введите числа: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+
+Console.WriteLine(number1);
+Console.WriteLine(number1);
