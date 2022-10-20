1bf3e3d2ccbe5e8dda00321a946643524437e3f6
Dmitry Saenko <dm.saenko@gmail.com>
2022-10-20 15:43:48 +0300

    programm ready

--- Lesson1/Program.cs
+++ Lesson1/Program.cs
@@ -5,7 +5,25 @@
 Console.WriteLine("Введите числа: ");
 int number1 = Convert.ToInt32(Console.ReadLine());
 int number2 = Convert.ToInt32(Console.ReadLine());
+    if (number1 > number2)
+        {
+            Console.WriteLine(number1);
+        }
+
+    if  (number2 > number1)
+        {
+            Console.WriteLine(number2);
+        }
+    if (number1 == number2)
+        {
+            Console.WriteLine("Числа равны");
+        }
+        
+     
+        
+                  
+        
+
+    
 
 
-Console.WriteLine(number1);
-Console.WriteLine(number1);
